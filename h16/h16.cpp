b2b6fcbd71fda6b58f6d7d4941dcf1e2a36d0c67
Krystal Lee <klee159@student.cccd.edu>
2019-10-12 23:55:08 +0000

    auto commit 10/12/19 23:55:08

--- h16/h16.cpp
+++ h16/h16.cpp
@@ -22,7 +22,7 @@ void get(std::istream& in, Point& p)
 void print(std::ostream& out, const Point& p)
 {
 
-    out << "(" << p.x << ", " << p.y << ")";
+    out << "(" << p.x << "," << p.y << ")";
 }
 
 double distanceBetween(const Point& a, const Point& b)
