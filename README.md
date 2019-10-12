a4ac7e0584beeb60c7232f10e8eed8683b426b5a
ldcole08 <54421501+ldcole08@users.noreply.github.com>
2019-10-12 16:21:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# f19-hw-starters
+Starter Code for CS 150 Fall 2019
---
+++ h05/h05.cpp
@@ -0,0 +1,23 @@
+/**
+ *  @author Put your name here
+ *  @date Put the date here
+ *  @file h05.cpp
+ */
+#include <string>
+#include <cmath>
+using namespace std;
+
+string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+
+#include "h05.h"
+
+// Place the implementation (definition) of your function here
+
+
+
+
+/////////////// Optional Student Code /////////////////
+int run()
+{
+    return 0;
+}
