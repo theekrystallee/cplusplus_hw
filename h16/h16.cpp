436549b209dd5c891add28a4328e7a518d0904b6
Krystal Lee <klee159@student.cccd.edu>
2019-10-12 23:30:17 +0000

    auto commit 10/12/19 23:30:17

--- h16/h16.cpp
+++ h16/h16.cpp
@@ -12,8 +12,42 @@ string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 #include "h16.h"
 // Add your implementation here
 
+void get(std::istream& in, Point& p)
+{
+    in >> p.x >> p.y;
+}
 
+/**
+ * Displays a Point in the form (x, y).
+ * @param out the stream to print on
+ * @param p the Point to print
+ */
+void print(std::ostream& out, const Point& p)
+{
+    out << "(" << p.x << ", " << p.y << ")";
+}
 
+double distanceBetween(const Point& a, const Point& b)
+{
+    int dx = a.x - b.x;
+    int dy = a.y - b.y;
+    return sqrt (dx * dx + dy * dy);
+}
+Point midpoint(const Point& a, const Point& b)
+{
+    struct Point mid;
+    mid.x = (a.x + b.x) / 2;
+    mid.y = (a.y + b.y) / 2;
+    return mid;
+}
+double perimeter(const Triangle& t)
+{
+    double perim = 0.0;
+    perim += distanceBetween(t.p1, t.p2);
+    perim += distanceBetween(t.p2, t.p3);
+    perim += distanceBetween(t.p3, t.p1);
+    return perim;
+}
 /////////////// STUDENT TESTING ////////////////////
 int run()
 {
