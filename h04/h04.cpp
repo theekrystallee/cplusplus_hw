0d8f89eeadc2f6bb23c60dc57dfaa039862bc1a4
Krystal Lee <klee159@student.cccd.edu>
2019-10-14 22:08:36 +0000

    auto commit 10/14/19 22:08:36

--- h04/h04.cpp
+++ h04/h04.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Krystal Lee
+ *  @date Fall 2019 MW/AM
  *  @file h04.cpp
  */
 #include <iostream>
@@ -8,14 +8,14 @@
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "klee159"; // Add your Canvas/occ-email ID
 extern string ASSIGNMENT;
 
 // Add your function declaration here
-string getStatus(double gpa, int credits, int honorsCredits);
+// string getStatus(double gpa, int credits, int honorsCredits);
 
 /**
- * Describe the purpose of your program here.
+ * A program that calculates your graduation status.
  * @return 0 for success.
  */
 int run()
@@ -31,11 +31,48 @@ int run()
 	cin >> gpa >> credits >> honorsCredits;
 
 	// You will write this function
-	string result = getStatus(gpa, credits, honorsCredits);
+	// string result = getStatus(gpa, credits, honorsCredits);
 
-	cout << "Result is [\"" << result << "\"]" << endl;
+	string summa = "summa cum laude";
+	string magna = "magna cum laude";
+	string cumLaude = "cum laude";
+	string grad = "graduating";
+	string notGrad = "not graduating";
 
+	if (gpa < 2.0 && credits < 180 && honorsCredits < 17)
+	{
+		cout << "Result is [\"" << notGrad << "\"]" << endl;
+	}
+	if (gpa > 2.0 && credits < 180 && honorsCredits < 31)
+	{
+		cout << "Result is [\"" << notGrad << "\"]" << endl;
+	}
+	if (gpa < 2.0 && credits > 180 && honorsCredits < 31)
+	{
+		cout << "Result is [\"" << notGrad << "\"]" << endl;
+	}
+	if (gpa >= 2.0 && gpa < 3.6 && credits >= 180)
+	{
+		cout << "Result is [\"" << grad << "\"]" << endl;
+	}
+	if (gpa >= 3.6 && gpa < 3.8 && credits >= 180 && honorsCredits < 15)
+	{
+		cout << "Result is [\"" << cumLaude << "\"]" << endl;
+	}
+	if (gpa >= 3.8 && credits >= 180 && honorsCredits < 15)
+	{
+		cout << "Result is [\"" << magna << "\"]" << endl;
+	}
+	else if (gpa >= 3.6 && gpa < 3.8 && credits >= 180 && honorsCredits >= 15)
+	{
+		cout << "Result is [\"" << magna << "\"]" << endl;
+	}
+	else if (gpa >= 3.8 && credits >= 180 && honorsCredits >= 15)
+	{
+		cout << "Result is [\"" << summa << "\"]" << endl;
+	}
     return 0;
 }
 
 // Implement your function here
+
