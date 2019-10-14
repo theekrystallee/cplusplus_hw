8dfa5fa8d7f81a167a788a9cbc829350a3753102
Krystal Lee <klee159@student.cccd.edu>
2019-10-14 22:12:08 +0000

    auto commit 10/14/19 22:12:08

--- h02/h02.cpp
+++ h02/h02.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Krystal Lee
+ *  @date Fall 2019 MW/AM
  *  @file h02.cpp
  */
 #include <iostream>
@@ -8,18 +8,45 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "Who Are You?";  // Add your Canvas login name
+string STUDENT = "klee159";  // Add your Canvas login name
 extern string ASSIGNMENT;
 
 /**
- * One line describing what this program does.
+ * A program that calculates how much diet soda can be consumed before it becoming a lethal dose.
  * @return 0 for success.
  */
 int run()
 {
-    // Add your implementation comments here
+    cout << STUDENT << "-" << ASSIGNMENT << ": ";
+    cout << "A Lethal Dose" << endl;
+    cout << string(50, '-') << endl;
 
-    // Write your code here
+    // Input
+    double mouseWeight;
+    cout << "Weight of the mouse in grams: ";
+    cin >> mouseWeight;
+
+    double lethal;
+    cout << "Lethal dose for the mouse (in grams): ";
+    cin >> lethal;
+
+    double desiredWeight;
+    cout << "Desired weight of the dieter (in pounds): ";
+    cin >> desiredWeight;
+
+    // Processing
+    const double SODA_GRAMS = 350.0;
+    const double POUND = 454.0;
+    const double PERCENT_SWEETENER = 0.001;
+
+    double numLethalDose = desiredWeight * POUND * (lethal / mouseWeight);
+    double numLethalCans = numLethalDose / (PERCENT_SWEETENER * SODA_GRAMS);
+
+    // Output
+    cout << fixed << setprecision(0);
+    cout << "Lethal dose in grams, cans is "
+    << "[" <<  numLethalDose << ", " << numLethalCans
+    << "]" << endl;
 
     return 0;
 }
--- h05/h05.cpp
+++ h05/h05.cpp
@@ -13,14 +13,6 @@ string STUDENT = "klee159"; // Add your Canvas/occ-email ID
 
 #include "h05.h"
 
-/**
- * Calculates the focal length of a lens.
- * @param d thickness of the lens
- * @param r1 radii r1
- * @param r2 radii r2
- * @param n the refractive index
- * @return the focal length
- */
 double focalLength(double d, double r1, double r2, double n)
 {
     double f;
