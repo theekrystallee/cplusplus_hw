a671b28bc46a8cb518d4044cd54f7bda8e4dc5bd
Krystal Lee <klee159@student.cccd.edu>
2019-10-14 21:59:05 +0000

    auto commit 10/14/19 21:59:05

--- h11/h11.cpp
+++ h11/h11.cpp
@@ -1,18 +1,56 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Krystal Lee
+ *  @date Fall 2019 MW-AM
  *  @file h11.cpp
  */
 #include <string>
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+#include "h11.h"
+
+string STUDENT = "klee159"; // Add your Canvas/occ-email ID
 
 // Write your function here
 void strip()
 {
+    char ch;
+    bool inString = false; // not inside a single line
+    bool inSingle = false; // not inside a multi line
+    bool inMulti = false; // not in string
 
+    while (cin.get(ch))
+    {
+        if (inSingle && ch == '\n')
+        {
+            inSingle = false;
+        }
+        else if (inString && ch == '"')
+        {
+            inString = false;
+        }
+        else if (inMulti && ch == '*' && cin.peek() == '/')
+        {
+            inMulti = false;
+            cin.get(ch);
+            cin.get(ch);
+        }
+        else if (!inSingle && !inMulti && !inString)
+        {
+            if (ch == '"') inString = true;
+            else if (ch == '/' && cin.peek() == '*') inMulti = true;
+            else if (ch == '/' && cin.peek() == '/') inSingle = true;
+        }
+        else if (inString && ch == '\\' && cin.peek() == '"')
+        {
+            cout.put(ch);
+            cin.get(ch);
+        }
+        if (cin && !inSingle && !inMulti)
+        {
+            cout.put(ch);
+        }
+    }
 }
 
 //////////////// STUDENT TESTING ////////////////////
