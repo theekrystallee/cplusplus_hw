ef23fbb0a8363c19bf60258056ee780e3a4181ad
Krystal Lee <klee159@student.cccd.edu>
2019-10-14 19:24:45 +0000

    auto commit 10/14/19 19:24:44

--- h09/h09.cpp
+++ h09/h09.cpp
@@ -1,19 +1,84 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Krystal Lee
+ *  @date Fall 2019 MW-AM
  *  @file h09.cpp
  */
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "klee159";
 
 #include "h09.h"
 
-// Put your function implementation (definitions) in this file
+string codeForDigit(int digit)
+{
+    string result;
+    switch(digit)
+    {
+        case 1: result = ":::||";
+            break;
 
+        case 2: result = "::|:|";
+            break;
 
+        case 3: result = "::||:";
+            break;
 
+        case 4: result = ":|::|";
+            break;
+
+        case 5: result = ":|:|:";
+            break;
+
+        case 6: result = ":||::";
+            break;
+
+        case 7: result = "|:::|";
+            break;
+
+        case 8: result = "|::|:";
+            break;
+
+        case 9: result  = "|:|::";
+            break;
+
+        case 0: result  = "||:::";
+            break;
+    }
+    return result;
+}
+
+int checkDigit(int zip)
+{
+
+    int sum = 0;
+
+    while (zip != 0)
+    {
+        sum += zip % 10;
+        zip /= 10;
+    }
+    if (sum % 10 == 0)
+    {
+        return 0;
+    }
+    sum = 10 - (sum % 10) % 10;
+    return sum;
+};
+
+string barCode(int zip)
+{
+    int i = checkDigit(zip);
+    string result = codeForDigit(i);
+    i = 0;
+    while (zip != 0)
+    {
+        i = zip % 10;
+        result =  codeForDigit(i) + result;
+        zip /= 10;
+    }
+    return + "|" + result + "|";
+}
 
 /////////// Student Testing ///////////////////////
 int run()
