09483e22614a946fcf270f714c0b7165a50f84ed
Krystal Lee <klee159@student.cccd.edu>
2019-10-14 22:22:03 +0000

    auto commit 10/14/19 22:22:03

--- h07/h07.cpp
+++ h07/h07.cpp
@@ -1,17 +1,36 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Krystal Lee
+ *  @date Fall 2019 MW-AM
  *  @file h07.cpp
  */
 #include <string>
 #include <cctype>
 using namespace std;
 
-string STUDENT = "WHO AM I?";  // Add your Canvas login name
+string STUDENT = "klee159";  // Add your Canvas login name
 
-// Implement your function here
-
-
+int sumNums(const string& s)
+{
+    int sum = 0;
+    int num = 0;
+    for (size_t i = 0, len = s.size(); i <= len - 1; i++)
+    {
+        char digit = s.at(i);
+        if (isdigit(digit))
+        {
+            digit -= '0';
+            num *= 10;
+            num += digit;
+        }
+        else
+        {
+            sum += num;
+            num = 0;
+        }
+    }
+    sum += num;
+    return sum;
+}
 
 /////////////// Optional Student Code /////////////////
 int run()
