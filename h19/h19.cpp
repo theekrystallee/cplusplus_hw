46157a80be4ce99f6f9acbdfd21c04de3bc310b6
Krystal Lee <klee159@student.cccd.edu>
2019-10-14 03:04:43 +0000

    auto commit 10/14/19 03:04:43

--- h19/h19.cpp
+++ h19/h19.cpp
@@ -11,14 +11,56 @@
 #include <cctype>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "klee159"; // Add your Canvas/occ-email ID
 
 
 #include "h19.h"
 
-// Place your function here
+/**
+    Reads any stream until end-of-file. Returns a vector of misspelled words,
+    but not those words that have been excluded.
+    @param in the stream to read from
+    @param dictionary vector of string containing correct-spelled words.
+    @param excluded vector of string containing words to ignore.
+    @return a vector of misspelled words, along with their position in the
+        original file.
+*/
+vector<WORD> spellCheck(istream& in,
+                    const vector<string>& dictionary,
+                    const vector<string>& excluded)
+{
+    vector<WORD> results;
+    string word;
+    long long position = 0;
 
+    //pos_type tellg() = -1;
+    while (true)
+    {
+        if (position == 0)
+        {
+            return results;
+            break;
+        }
+        in >> word >> ws;
+        // position (in.tellg()) static_cast<long long>);
+        // if (in.eof() && in.tellg())
+        // {
+        //     break;
+        // }
+        // in >> word >> ws;
 
+        // if (isalpha(word))
+        // {
+        //     tolower(word);
+
+        //     if (ispunct(static_cast<unsigned char>(word)))
+        //     {
+        //         word.erase();
+        //     }
+        // }
+
+    }
+}
 
 
 /////////////// STUDENT TESTING ////////////////////
