9d5d182692f7252e5db7c4190516cce18dfd1301
Krystal Lee <klee159@student.cccd.edu>
2019-10-14 22:00:25 +0000

    auto commit 10/14/19 22:00:25

--- h10/h10.cpp
+++ h10/h10.cpp
@@ -1,6 +1,6 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Krystal Lee
+ *  @date Fall 2019 MW-AM
  *  @file h10.cpp
  */
 #include <iostream>
@@ -8,11 +8,65 @@
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "klee159"; // Add your Canvas/occ-email ID
 
 #include "h10.h"
 
-// Place your function definitions in this file.
+bool read(const string& agePrompt, int& age, bool check)
+{
+    string junk;
+    cout << agePrompt;
+    cin >> age;
+    if (cin.fail())
+    {
+        cin >> junk;
+        cin.clear();
+        check = false;
+    }
+    else
+    {
+        check = true;
+    }
+    return check;
+}
+
+bool read(const string& gpaPrompt, double& gpa, bool check)
+{
+    string junk;
+    cout << gpaPrompt;
+    cin >> gpa;
+    if (cin.fail())
+    {
+        cin >> junk;
+        cin.clear();
+        check = false;
+    }
+    else
+    {
+        cin.ignore(1024, '\n');
+        check = true;
+    }
+    return check;
+
+}
+
+bool read(const string& namePrompt, string& name, bool check = true)
+{
+    cout << namePrompt;
+    getline(cin, name);
+    return check;
+
+}
+
+bool read(char& ch, char dot = '.')
+{
+    cin.get(ch);
+    if (ch == dot)
+    {
+        return false;
+    }
+    return true;
+}
 
 ////////////////// STUDENT TESTING /////////////
 int run()
---
+++ h11/h11.h
@@ -0,0 +1,14 @@
+#ifndef H11_H_
+#define H11_H_
+/**
+    @file h11.h
+    @author Krystal Lee
+    @version Fall 2019 MW-AM
+*/
+
+#include <string>
+
+// Place your prototypes here
+void strip(bool inSingle, bool inMulti, bool inString);
+
+#endif
