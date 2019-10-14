306fa5736eb72a02864854093b77a3c3929b2b63
Krystal Lee <klee159@student.cccd.edu>
2019-10-14 21:46:54 +0000

    auto commit 10/14/19 21:46:54

--- h15/h15.cpp
+++ h15/h15.cpp
@@ -4,9 +4,13 @@
  *  @file h15.cpp
  */
 #include <string>
+#include <iomanip>
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "klee159"; // Add your Canvas/occ-email ID
 
 // NO OTHER CODE IN THIS FILE
 // PUT EVERYTHING IN THE HEADER FILE
