ffc17c93a14d9513d952c3acc057243e187719de
Krystal Lee <klee159@student.cccd.edu>
2019-10-14 21:57:20 +0000

    auto commit 10/14/19 21:57:20

--- h12/h12.cpp
+++ h12/h12.cpp
@@ -1,19 +1,63 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Krystal Lee
+ *  @date Fall 2019 MW-AM
  *  @file h12.cpp
  */
 #include <iostream>
 #include <iomanip>
 #include <cctype>
+#include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "klee159"; // Add your Canvas/occ-email ID
 
 // Write your function here
 void expense()
 {
-    
+    char ch;
+    double num = 0.0;
+    double total = 0.0;
+
+    while (cin.peek() != EOF)
+    {
+        while (cin.get(ch))
+        {
+            if (isspace(ch) && isspace(cin.peek()))
+            {
+                while (isspace(cin.peek()))
+                {
+                    cin.get(ch);
+                }
+            }
+            if (isdigit(cin.peek()))
+            {
+                cout << ", ";
+                break;
+            }
+            cout.put(ch);
+        }
+
+        while (cin.get(ch))
+        {
+            if (isdigit(ch))
+            {
+                cin.unget();
+                cin >> num;
+            }
+            else if (ch == ' ' || ch == '\n')
+            {
+                total += num;
+                num = 0.0;
+            }
+            if (ch == '\n')
+            {
+                cout << fixed << setprecision(2);
+                cout << total << '\n';
+                total = 0.0;
+                break;
+            }
+        }
+    }
 }
 
 //////////////// STUDENT TESTING ////////////////////
