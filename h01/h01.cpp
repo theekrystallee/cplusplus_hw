9593b31f218c5da20dfe4fb5324b32ed50b64912
Krystal Lee <klee159@student.cccd.edu>
2019-10-14 22:16:39 +0000

    auto commit 10/14/19 22:16:39

--- h01/h01.cpp
+++ h01/h01.cpp
@@ -1,26 +1,79 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Krystal Lee
+ *  @date Fall 2019 MW/AM
  *  @file h01.cpp
  */
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <string>
+#include <cctype>
 using namespace std;
 
-string STUDENT = "Who Are You?";  // Add your Canvas login name
+string STUDENT = "klee159";  // Add your Canvas login name
 extern string ASSIGNMENT;
 
-
 /**
- * One line describing what this program does.
+ * A program that adds and subtracts time from user input.
  * @return 0 for success.
  */
 int run()
 {
-    // Add your implementation comments here
+    cout << STUDENT << "-" << ASSIGNMENT << ": ";
+    cout << "Time on My Hands" << endl;
+    cout << string(50, '-') << endl;
 
-    // Write your code here
+    // cout << "Give me a time (such as 3:57) and a duration\n"
+    // << "(such as 1:05), and I'll tell you the sum\n"
+    // << "(that is, the time that follows the given time\n"
+    // << "by the given duration), and difference (the time that\n"
+    // << "precedes the given time by that duration).\n" << endl;
+
+    // // Input
+
+    // cout << STUDENT << "-" << ASSISNGMENT << ": ";
+    // cout << "Time on My Hands" << endl;
+    // cout << string(50, '-') << endl;
+
+    cout << "Give me a time (such as 3:57) and a duration\n"
+    << "(such as 1:05), and I'll tell you the sum\n"
+    << "(that is, the time that follows the given time\n"
+    << "by the given duration), and difference (the time that\n"
+    << "precedes the given time by that duration).\n" << endl;
+
+    int timeHours;
+    int timeMinutes;
+    cout << "   Time: ";
+    cin >> timeHours;
+    cin.get();
+    cin >> timeMinutes;
+
+    int durationHours;
+    int durationMinutes;
+    cout << "   Duration: ";
+    cin >> durationHours;
+    cin.get();
+    cin >> durationMinutes;
+
+    // Processing
+    int timeTime = timeHours * 60 + timeMinutes;
+    int duration = durationHours * 60 + durationMinutes;
+    int after = timeTime + duration;
+    int before = timeTime - duration + 1440;
+    int afterHours = (after / 60 % 12 + 11) % 12 + 1;
+    int afterMinutes = after % 60;
+    int beforeHours = (before / 60 + 11) % 12 + 1;
+    int beforeMinutes = before % 60;
+
+    // Output
+    cout << endl;
+    cout << setfill('0');
+    cout << durationHours << ":" << setw(2) << durationMinutes
+    << " hours after, and before, "
+    << timeHours << ":" << setw(2) << timeMinutes
+    << " is [" << afterHours << ":" << setw(2) << afterMinutes
+    << ", " << beforeHours << ":" << setw(2) << beforeMinutes
+    << "]" << endl;
 
     return 0;
 }
