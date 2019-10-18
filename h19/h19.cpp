1106c2c4ca723daca42cbac54a3961edb4654aa5
Krystal Lee <klee159@student.cccd.edu>
2019-10-18 23:43:55 +0000

    auto commit 10/18/19 23:43:55

--- h19/h19.cpp
+++ h19/h19.cpp
@@ -48,7 +48,7 @@ vector<WORD> spellCheck(istream& in,
 
         for (size_t i = 0; i < word.size(); i++)
         {
-            if (isalpha(word.at(i)) && (!ispunct(word.at(i))) && isupper(word.at(i)))
+            if (isalpha(word.at(i)) || (!ispunct(word.at(i))) || isupper(word.at(i)))
             {
                 word.at(i) = tolower(word.at(i));
 
@@ -59,6 +59,8 @@ vector<WORD> spellCheck(istream& in,
                 //         break;
                 //     }
                 // }
+
+
             }
 
         }
