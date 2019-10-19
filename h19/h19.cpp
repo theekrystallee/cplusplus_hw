eb2492eab22f6b7ec8c9dde8b94804298171fc88
Krystal Lee <klee159@student.cccd.edu>
2019-10-19 20:32:30 +0000

    auto commit 10/19/19 20:32:30

--- h19/h19.cpp
+++ h19/h19.cpp
@@ -98,17 +98,17 @@ vector<WORD> spellCheck(istream& in,
         // ElseIf not found
         // Search the dictionary->found
         // If found word Then (Not misspelled) Go to top of loop
-        if(! found)
-        {
-            for(size_t i = 0; i < dictionary.size(); ++i)
-            {
-                if(dictionary.at(i) == word)
-                {
-                    found = true;
-                    continue;
-                }
-            }
-        }
+        // if(! found)
+        // {
+        //     for(size_t i = 0; i < dictionary.size(); ++i)
+        //     {
+        //         if(dictionary.at(i) == word)
+        //         {
+        //             found = true;
+        //             continue;
+        //         }
+        //     }
+        // }
         // ElseIf not found Create a WORD, populate with word, position
         // Add new WORD to results
         // End Loop
