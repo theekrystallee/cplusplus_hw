8050be634840b17fb2b8d191cd8d3966a96557da
Krystal Lee <klee159@student.cccd.edu>
2019-10-19 06:06:51 +0000

    auto commit 10/19/19 06:06:51

--- h19/h19.cpp
+++ h19/h19.cpp
@@ -52,7 +52,6 @@ vector<WORD> spellCheck(istream& in,
             if (ispunct(word.at(i)))
             {
                 word.erase(i--, 1);
-                continue;
             }
         //     else if (word == word)
         //     {
