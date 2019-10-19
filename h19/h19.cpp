761599e9af94cebaa2083dca0bb432a06180dbe0
Krystal Lee <klee159@student.cccd.edu>
2019-10-19 01:02:04 +0000

    auto commit 10/19/19 01:02:04

--- h19/h19.cpp
+++ h19/h19.cpp
@@ -56,7 +56,7 @@ vector<WORD> spellCheck(istream& in,
                     //in.ignore(1024, '\n');
                     continue;
 
-                     if (ispunct(word.at(i)))
+                     if (ispunct(word.at(i)) && word.find("A"))
                     {
                         word.at(i) = toupper(word.at(i));
                         continue;
