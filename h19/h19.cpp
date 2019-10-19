cbe38a56ae9aa1582a0a8ed0100e6676deb71797
Krystal Lee <klee159@student.cccd.edu>
2019-10-19 05:18:48 +0000

    auto commit 10/19/19 05:18:48

--- h19/h19.cpp
+++ h19/h19.cpp
@@ -142,16 +142,16 @@ vector<WORD> spellCheck(istream& in,
         // }
 
         bool found = false;
-        for (size_t i = 0; i < results.size(); i++)
-        {
-            if (results.at(i).word == word)
-            {
-                results.at(i).positions.push_back(pos);
-                found = true;
-                break;
-            }
+        // for (size_t i = 0; i < results.size(); i++)
+        // {
+        //     if (results.at(i).word == word)
+        //     {
+        //         results.at(i).positions.push_back(pos);
+        //         found = true;
+        //         break;
+        //     }
 
-        }
+        // }
         // if (found)
         // {
         //     for (size_t i = 0; i < word.size(); i++)
