4b3b9d36aee200a300a189540ab3bd47cb743e22
Krystal Lee <klee159@student.cccd.edu>
2019-10-19 05:40:13 +0000

    auto commit 10/19/19 05:40:13

--- h19/h19.cpp
+++ h19/h19.cpp
@@ -71,15 +71,15 @@ vector<WORD> spellCheck(istream& in,
             {
                 word.at(j++) = word.at(i);
             }
-            // if (!ispunct(word.at(i)) || !isupper(word.at(i)) || (isspace(word.at(i)) && (ispunct(word.at(i)))))
-            // {
-            //     word.at(i) = tolower(word.at(i));
-            //     continue;
-            // }
-            // if (ispunct(word.at(i)))
-            // {
-            //     word.erase(i--, 1);
-            // }
+            if (!ispunct(word.at(i)) || !isupper(word.at(i)) || (isspace(word.at(i)) && (ispunct(word.at(i)))))
+            {
+                word.at(i) = tolower(word.at(i));
+                continue;
+            }
+            if (ispunct(word.at(i)))
+            {
+                word.erase(i--, 1);
+            }
 
                 //  if (ispunct(word.at(i)) && word.find('A'))
                 //     {
