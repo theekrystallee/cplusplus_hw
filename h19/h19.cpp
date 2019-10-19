624e312000f9a7b0545e1224f05177c6d57a81b9
Krystal Lee <klee159@student.cccd.edu>
2019-10-19 00:07:30 +0000

    auto commit 10/19/19 00:07:30

--- h19/h19.cpp
+++ h19/h19.cpp
@@ -50,10 +50,11 @@ vector<WORD> spellCheck(istream& in,
         {
             if (isalpha(word.at(i)) && (ispunct(word.at(i))) && isupper(word.at(i)))
             {
-                if (isupper(word.at(i)) && !isalpha(word.at(0)) && !isupper(word.at(len - 1)))
-                {
-                    word.at(i) = tolower(word.at(i));
-                }
+                word.at(i) = tolower(word.at(i));
+                // if (isupper(word.at(i)) && !isalpha(word.at(0)) && !isupper(word.at(len - 1)))
+                // {
+                //     word.at(i) = tolower(word.at(i));
+                // }
 
                 // for (i = 0; i > word.size(); i++)
                 // {
