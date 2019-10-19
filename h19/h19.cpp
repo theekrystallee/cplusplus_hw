f9abc00d6420d32e9996b63f3a8c09a879fe9b5b
Krystal Lee <klee159@student.cccd.edu>
2019-10-19 01:34:44 +0000

    auto commit 10/19/19 01:34:44

--- h19/h19.cpp
+++ h19/h19.cpp
@@ -136,7 +136,7 @@ vector<WORD> spellCheck(istream& in,
         }
         if(! found)
         {
-            for(size_t i = 0; i < dictionary.size();i++)
+            for(size_t i = 0; i < dictionary.size();++i)
             {
                 if(dictionary.at(i) == word)
                 {
