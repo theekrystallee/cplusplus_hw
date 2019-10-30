caf8069d706eec7f2e35c2ba65d8d5b972744a8c
Krystal Lee <klee159@student.cccd.edu>
2019-10-30 06:24:14 +0000

    auto commit 10/30/19 06:24:14

--- h23/h23.cpp
+++ h23/h23.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "klee159"; // Add your Canvas/occ-email ID
 
 #include "h23.h"
 
